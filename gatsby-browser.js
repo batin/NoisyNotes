deb36e7ebbe8d203d4e2a4af9aa3166bc81cac89
Batin Eryilmaz <batineryilmaz@icloud.com>
2020-06-01 13:20:36 +0300

    deploy problems fixed

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { AuthProvider } from "./src/services/auth"
+import { AuthProvider } from "./src/services/auth.js"
 
 export const wrapRootElement = ({ element }) => {
   return <AuthProvider>{element}</AuthProvider>
--- gatsby-ssr.js
+++
@@ -1,6 +0,0 @@
-const React = require("react")
-const GlobalContextProvider = require("./src/services/auth").default
-
-exports.wrapRootElement = ({ element }) => {
-  return <GlobalContextProvider>{element}</GlobalContextProvider>
-}
